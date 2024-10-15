0c2ee3b60dab717c43bc3ccd657939a9c5d88025
Christian_Yemele <yemelechristian2@gmail.com>
2024-10-15 07:46:38 +0100

    feat(): init frontend with tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/*.{html,js,ts,tsx}"],
-  daisyui: {
-    themes: ["light", "dark", "cupcake"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
   plugins: [
     require('daisyui'),
   ],
 }
-
