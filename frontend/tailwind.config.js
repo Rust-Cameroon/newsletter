4de9d5d40fb595eed59217606af173efe00ad901
Christian_Yemele <yemelechristian2@gmail.com>
2024-10-16 08:20:22 +0100

    feat(): first stage of subscription form

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,10 +4,10 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
   },
   plugins: [
-    require('daisyui'),
+    require('daisyui'), 
   ],
 }
